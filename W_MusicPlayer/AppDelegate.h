b88492abd11290fdfee224c34ff3b642241397eb
wangxiaolei <1178384094@qq.com>
2019-03-08 16:37:02 +0800

    Initial Commit

---
+++ W_MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W_MusicPlayer
+//
+//  Created by wangxiaolei on 2019/3/8.
+//  Copyright © 2019年 wxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W_MusicPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  W_MusicPlayer
+//
+//  Created by wangxiaolei on 2019/3/8.
+//  Copyright © 2019年 wxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
